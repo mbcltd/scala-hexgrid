7a88fa6553e270bbaa463f142ca73261a2a16793
David Morgan-Brown <david@morgan-brown.com>
2014-08-07 12:05:34 +0100

    Added the other OddQ, EvenR, OddR coordinate types.

--- src/main/scala/hex/Coordinates.scala
+++ src/main/scala/hex/Coordinates.scala
@@ -5,15 +5,31 @@ object Cube {
 }
 
 case class Cube(x:Int, y:Int, z:Int) {
-  def toAxial = Axial(x, z)
-  def toEvenQ = EvenQ(x, z + (x + (x&1)) / 2 )
+  def toAxial = Axial( x, z )
+  def toEvenQ = EvenQ( x, z + (x + (x&1)) / 2 )
+  def toOddQ = OddQ( x, z + (x - (x&1)) / 2 )
+  def toEvenR = EvenR( x + (z + (z&1)) / 2, z )
+  def toOddR = OddR( x + (z - (z&1)) / 2, z )
 }
 
 case class Axial(q:Int, r:Int) {
-  def toCube = Cube(q, r)
+  def toCube = Cube( q, r)
 }
 
 case class EvenQ(q:Int, r:Int) {
-  def toCube = Cube(q, r - (q + (q&1)) / 2)
+  def toCube = Cube( q, r - (q + (q&1)) / 2)
 }
+
+case class OddQ(q:Int, r:Int) {
+  def toCube = Cube( q, r - (q - (q&1)) / 2)
+}
+
+case class EvenR(q:Int, r:Int) {
+  def toCube = Cube( q - (r + (r&1)) / 2, r )
+}
+
+case class OddR(q:Int, r:Int) {
+  def toCube = Cube( q - (r - (r&1)) / 2, r )
+}
+
 
--- src/test/scala/hex/CoordinatesSpec.scala
+++ src/test/scala/hex/CoordinatesSpec.scala
@@ -21,9 +21,27 @@ class CoordinatesSpec extends FlatSpec with Matchers  {
     }
   }
 
-  "Cube coordinates" should "map to and from EvenQ coordinates" in {
+  it should "map to and from Even Q coordinates" in {
     forAll( testCubes ) {
       (x: Int, z: Int) => Cube(x, z) shouldEqual Cube(x, z).toEvenQ.toCube
+    }
+  }
+
+  it should "map to and from Odd Q coordinates" in {
+    forAll( testCubes ) {
+      (x: Int, z: Int) => Cube(x, z) shouldEqual Cube(x, z).toOddQ.toCube
+    }
+  }
+
+  it should "map to and from Even R coordinates" in {
+    forAll( testCubes ) {
+      (x: Int, z: Int) => Cube(x, z) shouldEqual Cube(x, z).toEvenR.toCube
+    }
+  }
+
+  it should "map to and from Odd R coordinates" in {
+    forAll( testCubes ) {
+      (x: Int, z: Int) => Cube(x, z) shouldEqual Cube(x, z).toOddR.toCube
     }
   }
 
